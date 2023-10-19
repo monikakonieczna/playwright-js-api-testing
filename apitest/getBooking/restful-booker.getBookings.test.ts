878aa48e31b5a4780e42164c0182d03440e209fb
Monika Konieczna <konieczna.monika01@gmail.com>
2023-10-19 07:27:53 +0200

    Implemeted tests for get Booking

---
+++ apitest/getBooking/restful-booker.getBookings.test.ts
@@ -0,0 +1,33 @@
+import { expect, test } from "playwright/test";
+import bookingData = require("../../test-data/booking.json");
+
+test.beforeEach("Create Booking", async ({ request, baseURL }) => {
+  const response = await request.post(`${baseURL}/booking`, {
+    data: bookingData,
+  });
+  expect(response.status()).toBe(200);
+  expect(response.ok()).toBeTruthy();
+
+  const responseBody = await response.json();
+  process.env.bookingID = responseBody.bookingid;
+});
+
+test("Get BookingIds", async ({ request, baseURL }) => {
+  const response = await request.get(`${baseURL}/booking`);
+  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(200);
+
+  let responseAsString = await response.body().then((b) => {
+    let data = JSON.parse(b.toString());
+    return data.filter(
+      (d: { bookingid: string | undefined }) =>
+        d.bookingid == process.env.bookingID
+    );
+  });
+
+  let ID = process.env.bookingID;
+  const a = ID;
+  if (a) {
+    expect(responseAsString[0]).toHaveProperty("bookingid", +a);
+  }
+});
