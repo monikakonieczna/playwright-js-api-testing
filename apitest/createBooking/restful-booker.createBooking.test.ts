5fd5a2efe58b69b9c3a8255af21ff4268c9d89ec
Monika Konieczna <konieczna.monika01@gmail.com>
2023-10-20 05:41:27 +0200

    Implemented test for post method

--- apitest/createBooking/restful-booker.createBooking.test.ts
+++ apitest/createBooking/restful-booker.createBooking.test.ts
@@ -1,20 +1,21 @@
 import { expect, test } from "playwright/test";
+import bookingData = require('../../test-data/post-booking.json');
 
-//Create
-test("Create Booking", async ({ request, baseURL }) => {
-    const _response = await request.post(`${baseURL}/booking`, {
-        data: {
-            "firstname": "Sally",
-            "lastname": "Brown",
-            "totalprice": 111,
-            "depositpaid": true,
-            "bookingdates": {
-                "checkin": "2013-02-23",
-                "checkout": "2014-10-23"
-            },
-            "additionalneeds": "Breakfast"
-        }
+test.only("Create Booking", async ({ request, baseURL }) => {
+    const response = await request.post(`${baseURL}/booking`, {
+        data: bookingData
     });
-    expect(_response.status()).toBe(200);
-    expect(_response.ok()).toBeTruthy();
+
+    const responseBody = await response.json();
+    process.env.bookingID= responseBody.bookingid;
+
+    console.log(responseBody);
+    expect(response.status()).toBe(200);
+    expect(response.ok()).toBeTruthy();
+    expect(responseBody.booking).toHaveProperty("firstname", bookingData.firstname);
+    expect(responseBody.booking).toHaveProperty("lastname", bookingData.lastname);
+    expect(responseBody.booking).toHaveProperty("totalprice", bookingData.totalprice);
+    expect(responseBody.booking).toHaveProperty("depositpaid", bookingData.depositpaid);
+    expect(responseBody.booking).toHaveProperty("additionalneeds", bookingData.additionalneeds);
 })
+
